ed09bfab177cb5cdcbf29ad7cc7331dc1454a328
João Figueiredo <joao.ggwp@outlook.com>
2024-11-10 22:17:45 -0300

    feat: redirect user to dashboard after login
      and if user is logged, show your username in header

--- src/app/layout/header.tsx
+++ src/app/layout/header.tsx
@@ -1,28 +1,44 @@
 "use client";
 
+import jwt from "jsonwebtoken";
 import Image from "next/image";
+import nookies from "nookies";
 import { useState } from "react";
-import logo from '../favicon.ico';
+import logo from "../favicon.ico";
 import Signin from "../signin/page";
 
 export default function Header() {
   const [isOpen, setOpen] = useState(false);
+  const token = nookies.get();
+  console.log(token);
+  let decodedToken = jwt.decode(token.authToken);
+  if (!decodedToken) decodedToken = "user";
 
   function openModal(): void {
     setOpen(!isOpen);
-    console.log(isOpen)
   }
 
   return (
     <div className="m-0 p-0 flex flex-col items-center">
-      <header className=" flex justify-between items-center pt-[30px] pb-[30px] px-[10%]">
+      <header className=" flex justify-between w-full items-center pt-[30px] pb-[30px] px-[10%]">
         <Image src={logo} alt="logo" />
         <nav>
-        <ul>
-          <li className=" font-medium text-base"> <a className=" font-medium text-base" href="/about">About this project</a></li>
-        </ul>
+          <ul>
+            <li className=" font-medium text-base">
+              {" "}
+              <a className=" font-medium text-base" href="/about">
+                About this project
+              </a>
+            </li>
+          </ul>
         </nav>
-        <button className=" font-medium text-base" onClick={openModal}>Sign in</button>
+        {!token ? (
+          <button className=" font-medium text-base" onClick={openModal}>
+            Sign in
+          </button>
+        ) : (
+          <h1>{decodedToken.username}</h1>
+        )}
       </header>
       <Signin isOpen={isOpen} />
     </div>
